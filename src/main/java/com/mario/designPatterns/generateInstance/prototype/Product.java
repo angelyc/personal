1cb63d8e36cd49d7924a7240fb35359e50a25917
liuyc <liuyc@mingdutech.com>
2020-03-09 14:14:34 +0800

    design pattern: prototype description

--- src/main/java/com/mario/designPatterns/generateInstance/prototype/Product.java
+++ src/main/java/com/mario/designPatterns/generateInstance/prototype/Product.java
@@ -1,5 +1,12 @@
 package com.mario.designPatterns.generateInstance.prototype;
 
+
+/**
+ * Prototype模式，根据实例生成新实例
+ * Prototype有“原型”“模型”的意思，
+ * 在设计模式中，Prototype是指根据实例原型、实例模型来生成新的实例
+ * Java中，可以使用clone创建出实例的副本。
+ * */
 public interface Product extends Cloneable {
     void use(String s);
     Product createClone();
