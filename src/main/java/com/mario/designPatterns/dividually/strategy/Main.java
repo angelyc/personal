688d3673c9c25c92b32030740ea7d8a91bd8895d
liuyc <liuyc@mingdutech.com>
2020-03-13 16:38:16 +0800

    design pattern: strategy 策略模式

--- src/main/java/com/mario/designPatterns/dividually/strategy/Main.java
+++ src/main/java/com/mario/designPatterns/dividually/strategy/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Player a = new Player("Taro", new WinnerStrategy(314));
         Player b = new Player("Hana", new ProbStrategy(15));
+        Player c = new Player("Tom", new RandomStrategy(666));
 
         for (int i = 0; i < 10000; i++) {
             Hand nextHand1 = a.nextHand();
@@ -25,5 +26,27 @@ public class Main {
         System.out.println("Total result: ");
         System.out.println(a.toString());
         System.out.println(b.toString());
+
+
+        for (int i = 0; i < 10000; i++) {
+            Hand nextHand1 = a.nextHand();
+            Hand nextHand2 = c.nextHand();
+            if (nextHand1.isStrongerThan(nextHand2)) {
+                System.out.println("Winner: " + a);
+                a.win();
+                c.lose();
+            } else if (nextHand2.isStrongerThan(nextHand1)) {
+                System.out.println("Winner: " + c);
+                c.win();
+                a.lose();
+            } else {
+                System.out.println("Even... ");
+                a.even();
+                c.even();
+            }
+        }
+        System.out.println("Total result: ");
+        System.out.println(a.toString());
+        System.out.println(c.toString());
     }
 }
--- src/main/java/com/mario/designPatterns/dividually/strategy/ReadMe.md
+++ src/main/java/com/mario/designPatterns/dividually/strategy/ReadMe.md
@@ -9,3 +9,12 @@ ConcreteStrategy角色负责实现Strategy角色的接口（API），即负责�
 # Context 上下文
 
 负责使用Strategy角色，Context角色保存了ConcreteStrategy角色的实例，并使用ConcreteStrategy角色去实现需求
+
+# 相关设计模式
+
+- 使用Strategy模式可以整体替换算法
+- 使用Abstract Factory模式可以整体替换具体工厂、零件和产品。
+
+# 后续
+
+使用策略模式实现各种排序方法
